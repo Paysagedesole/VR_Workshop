b9d656e1a62f916eac7e114a220c9533069dfcf1
Paysagedesole <58518206+Paysagedesole@users.noreply.github.com>
2019-12-13 15:37:55 +0100

    SetupBrothel

---
+++ Assets/FishMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FishMove : MonoBehaviour {
+    public Transform firstLocation;
+    public Transform secondLocation;
+    public NavMeshAgent agent;
+
+    public Transform destination;
+    // Use this for initialization
+    void Start () {
+        //agent.SetDestination(Random.ve)
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+
+    
+    
+}
---
+++ Assets/Scripts/travelToPoints.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class travelToPoints : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshPath path;
+    public ParticleSystem particleRef;
+    
+    void Start()
+    {
+        path = new NavMeshPath();
+        NavMesh.CalculatePath(transform.position, target.position, NavMesh.AllAreas, path);
+        for(int i = 0; i < path.corners.Length -1; i++)
+        {
+            ParticleSystem newParticle = Instantiate(particleRef, new Vector3 (path.corners[i].x, path.corners[i].y+1, path.corners[i].z) , new Quaternion(0,0,0,1 ));
+            if (path.corners[i + 1] != null)
+            {
+                newParticle.transform.LookAt(path.corners[i + 1]);
+                float hypotenuseLength = Vector2.Distance(new Vector2(path.corners[i].x, path.corners[i].z), new Vector2(path.corners[i + 1].x, path.corners[i + 1].z));
+                float oppositeLength = path.corners[i + 1].z - path.corners[i].z;
+                float sinHorizontal = oppositeLength / hypotenuseLength;
+                Debug.Log(sinHorizontal);
+                ///newParticle.startRotation3D = new Vector3 (0 , 0, 0);
+            }
+        }
+    }
+
+    void Update()
+    {
+   
+        
+    }
+    
+}
