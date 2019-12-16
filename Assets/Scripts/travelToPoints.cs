f9ca8f7273da00a3699297093233a36a85d8e99e
Paysagedesole <58518206+Paysagedesole@users.noreply.github.com>
2019-12-16 15:03:10 +0100

    Setup2

--- Assets/Scripts/travelToPoints.cs
+++ Assets/Scripts/travelToPoints.cs
@@ -15,13 +15,18 @@ public class travelToPoints : MonoBehaviour
         {
             ParticleSystem newParticle = Instantiate(particleRef, new Vector3 (path.corners[i].x, path.corners[i].y+0.5f, path.corners[i].z) , new Quaternion(0,0,0,1 ));
             travelToNextPoint_Script newParticleScriptRef = newParticle.GetComponent<travelToNextPoint_Script>();
-            newParticleScriptRef.listOfPoints.Add (path.corners[i]);
+            for (int x = 0; x < path.corners.Length - 2; x++)
+            {
+                newParticleScriptRef.listOfPoints.Add(path.corners[x]);
+            }
+
+            newParticleScriptRef.actualPointIndex = i;
         }
     }
 
     void Update()
     {
-   
+    
         
     }
     
